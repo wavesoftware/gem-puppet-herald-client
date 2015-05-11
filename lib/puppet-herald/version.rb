f0c36653efce90bed1f029e8b9bab3d10153ea5a
Krzysztof Suszyński <krzysztof.suszynski@coi.gov.pl>
2015-05-11 12:26:44 +0200

    Pre-release version v1.0.0

--- lib/puppet-herald/version.rb
+++ lib/puppet-herald/version.rb
@@ -14,7 +14,7 @@ module PuppetHerald
   end
 
   # Version for Herald Client
-  VERSION     = version_prep '0.8.1'
+  VERSION     = version_prep '1.0.0.pre'
   # Lincense for Herald Client
   LICENSE     = 'Apache-2.0'
   # Project name
